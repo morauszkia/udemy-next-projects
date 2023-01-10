c3e0c00d63d4ceaad1276571e9f69a4dfb610f6f
András Morauszki <morauszki.andras@gmail.com>
2023-01-10 17:44:18 +0100

    feat(*): finished project

--- 01-next-events/pages/events/index.js
+++ 01-next-events/pages/events/index.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import { useRouter } from 'next/router';
+
+import { getAllEvents } from '../../dummy-data';
+import EventList from '../../components/events/event-list';
+import EventsSearch from '../../components/events/events-search';
 
 const AllEventsPage = () => {
+  const events = getAllEvents();
+
+  const router = useRouter();
+
+  const findEventsHandler = (year, month) => {
+    const fullPath = `/events/${year}/${month}`;
+
+    router.push(fullPath);
+  };
+
   return (
-    <div>
-      <h1>All Events</h1>
-    </div>
+    <>
+      <EventsSearch onSearch={findEventsHandler} />
+      <EventList items={events} />
+    </>
   );
 };
 
